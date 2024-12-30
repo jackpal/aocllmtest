084d4c7f86e0032b8f8b193e5628e9570de4c7a4
Jack Palevich <jack.palevich@gmail.com>
2024-12-30 08:34:08 -0800

    Add gemma2:2b and gemma2:9b

--- ollama_driver.py
+++ ollama_driver.py
@@ -4,6 +4,8 @@ from typing import List, Tuple
 
 def models() -> List[str]:
     return [
+        'gemma2:2b',
+        'gemma2:9b',
         'gemma2:27b',
         'llama3.3',
         'qwen2.5-coder:32b',
@@ -25,3 +27,7 @@ def generate(model_name : str, prompt: str) -> Tuple[str, str]:
 
 def model_quota_timeout(model_name: str) -> int:
     return 10
+
+if __name__ == "__main__":
+    for model in models():
+        print(model, generate(model, 'what LLM model are you?'))
