b36d135ccff2f2e1eda3d3e0f11b251736d02af9
Jack Palevich <jack.palevich@gmail.com>
2024-12-30 14:31:12 -0800

    Fix highlighting, remove broken "remaining" table.

--- web_server.py
+++ web_server.py
@@ -100,81 +100,6 @@ def calculate_summary_data():
     conn.close()
     return summary_data, totals, model_families, models
 
-def calculate_remaining_percentages():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    # Get all model families and models
-    cursor.execute("SELECT model_family, model_name FROM Models")
-    models_data = cursor.fetchall()
-
-    # Get the total number of possible experiments (Part 1 puzzles only)
-    total_possible_experiments = 0
-    cursor.execute("SELECT COUNT(DISTINCT puzzle_year) FROM Experiments")
-    num_years = cursor.fetchone()[0]
-    cursor.execute("SELECT COUNT(DISTINCT puzzle_day) FROM Experiments WHERE puzzle_day <> 25")
-    num_days = cursor.fetchone()[0]
-    total_possible_experiments = num_years * num_days  # Only count Part 1 puzzles
-
-    remaining_percentages = {}
-
-    for row in models_data:
-        model_family = row['model_family']
-        model_name = row['model_name']
-
-        # Get the number of Part 1 puzzles attempted (correct or incorrect)
-        cursor.execute("""
-            SELECT COUNT(*)
-            FROM (
-                SELECT DISTINCT puzzle_year, puzzle_day
-                FROM Experiments
-                WHERE model_family = ? AND model_name = ? AND puzzle_part = 1
-                AND NOT answer_is_correct = 1
-                UNION
-                SELECT DISTINCT puzzle_year, puzzle_day
-                FROM Experiments
-                WHERE model_family = ? AND model_name = ? AND puzzle_part = 1
-                AND answer_is_correct = 1
-                AND NOT EXISTS (SELECT 1 from Experiments
-                                  WHERE model_family = ?
-                                  AND model_name = ?
-                                  AND puzzle_year = Experiments.puzzle_year
-                                  AND puzzle_day = Experiments.puzzle_day
-                                  AND puzzle_part = 2)
-            ) AS distinct_part1_attempts
-        """, (model_family, model_name, model_family, model_name, model_family, model_name))
-        part1_available_count = cursor.fetchone()[0]
-
-        # Get the number of Part 2 puzzles attempted where Part 1 was successful
-        cursor.execute("""
-            SELECT COUNT(*)
-            FROM (
-                SELECT DISTINCT e2.puzzle_year, e2.puzzle_day
-                FROM Experiments e1
-                INNER JOIN Experiments e2
-                    ON e1.model_family = e2.model_family
-                    AND e1.model_name = e2.model_name
-                    AND e1.puzzle_year = e2.puzzle_year
-                    AND e1.puzzle_day = e2.puzzle_day
-                WHERE e1.model_family = ?
-                AND e1.model_name = ?
-                AND e1.puzzle_part = 1
-                AND e1.answer_is_correct = 1
-                AND e2.puzzle_part = 2
-            ) AS valid_part2_attempts
-        """, (model_family, model_name))
-        part2_attempted_count = cursor.fetchone()[0]
-
-        remaining_count = part1_available_count
-        remaining_percentage = (remaining_count / total_possible_experiments) * 100 if total_possible_experiments > 0 else 0
-
-        if model_family not in remaining_percentages:
-            remaining_percentages[model_family] = {}
-        remaining_percentages[model_family][model_name] = remaining_percentage
-
-    conn.close()
-    return remaining_percentages
-
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -213,7 +138,6 @@ def index():
     year_ranks = cursor.fetchall()
 
     summary_data, totals, model_families, models = calculate_summary_data()
-    remaining_percentages = calculate_remaining_percentages()
 
     conn.close()
 
@@ -229,8 +153,7 @@ def index():
         summary_data=summary_data,
         totals=totals,
         model_families=model_families,
-        models=models,
-        remaining_percentages=remaining_percentages
+        models=models
     )
 
 if __name__ == '__main__':
